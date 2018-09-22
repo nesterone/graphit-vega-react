ac2a2aeccc6c66dc00cfb3caa1e4819ea9a3ed42
Igor Nesterenko <igornester@gmail.com>
2018-09-22 19:05:13 +0300

    - compile spec and run react-vega, not rendered

--- src/vega-lite/VegaLite.js
+++ src/vega-lite/VegaLite.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import merge from 'lodash.merge'
+import Vega from 'react-vega';
+import { compile } from 'vega-lite';
+
 
 export const VegaLiteSpecContext = React.createContext();
 
@@ -21,20 +24,31 @@ export default class VegaLite extends React.Component {
             updateSpec: this.updateSpec
         };
 
+        this.state = {
+            spec: null
+        };
+
         console.log('prep context', this.vegaContext);
     }
 
     componentDidMount () {
         // taddaa spec is ready here
-        console.log('init vega lite', this.spec);
+        console.log('vega-lite spec', this.spec);
+        let compiledSpec = compile(this.spec);
+        console.log('compiled spec', compiledSpec);
+        this.setState(()=> ({
+            spec: compiledSpec
+        }))
     }
 
     render() {
         console.log('before render ', this.vegaContext);
         console.log('before render ', this.spec);
+        const specToRender = this.state.spec;
         return (
             <VegaLiteSpecContext.Provider value={this.vegaContext}>
                 {this.props.children}
+                {specToRender && <Vega spec={specToRender} /> }
             </VegaLiteSpecContext.Provider>
         );
     }
